225c6babc27926de41d3020214bf5235493569fe
Jim Kang <jimkang@gmail.com>
2017-03-10 12:30:19 -0500

    Implemented execute for move action.

--- actions/move.js
+++ actions/move.js
@@ -1,9 +1,11 @@
 var callNextTick = require('call-next-tick');
 var flatten = require('lodash.flatten');
 var findWhere = require('lodash.findwhere');
+var sb = require('standard-bail')();
 
 var move = {
-  canDo: canDoMove
+  canDo: canDoMove,
+  execute: executeMove
 };
 
 function canDoMove({actor, actionOpts, figureTree}, done) {
@@ -23,6 +25,27 @@ function canDoMove({actor, actionOpts, figureTree}, done) {
       minY: figure.minY + 0.25 + actionOpts.vector[1],
       maxY: figure.maxY - 0.25 + actionOpts.vector[1]
     };
+  }
+}
+
+function executeMove({actor, actionOpts, figureTree}, done) {
+  canDoMove({actor, actionOpts, figureTree}, sb(execute, done));
+
+  function execute(isDoable) {
+    if (!isDoable) {
+      done(null, false);
+    }
+    else {
+      actor.figures.forEach(updatePosition);
+      done(null, true);
+    }
+  }
+
+  function updatePosition(figure) {
+    figure.minX += actionOpts.vector[0];
+    figure.maxX += actionOpts.vector[0];
+    figure.minY += actionOpts.vector[1];
+    figure.maxY += actionOpts.vector[1];
   }
 }
 
---
+++ tests/move-action-tests.js
@@ -0,0 +1,211 @@
+var test = require('tape');
+var move = require('../actions/move');
+var assertNoError = require('assert-no-error');
+var rbush = require('rbush');
+var pick = require('lodash.pick');
+
+var playerSoul = {
+  figures: [
+    {
+      id: 'figure-AqYpgsIk',
+      minX: 9,
+      minY: 4,
+      maxX: 10,
+      maxY: 5,
+      passable: false
+      // TODO: ghostStyle flag.
+    }
+  ]
+};
+
+var threeSidesFigureTreeItems = [
+  {
+    id: 'above',
+    minX: 9,
+    maxX: 10,
+    minY: 3,
+    maxY: 4,
+    passable: false
+  },
+  {
+    id: 'below',
+    minX: 9,
+    maxX: 10,
+    minY: 5,
+    maxY: 6,
+    passable: true
+  },
+  {
+    id: 'right',
+    minX: 10,
+    maxX: 11,
+    minY: 4,
+    maxY: 5,
+    passable: false
+  }
+];
+
+var figureTree = rbush(9);
+figureTree.load(threeSidesFigureTreeItems);
+
+var canDoTestCases = [
+  {
+    name: 'Move left',
+    opts: {
+      actor: playerSoul,
+      actionOpts: {
+        vector: [-1, 0]
+      },
+      figureTree: figureTree
+    },
+    expected: true
+  },
+  {
+    name: 'Move right',
+    opts: {
+      actor: playerSoul,
+      actionOpts: {
+        vector: [1, 0]
+      },
+      figureTree: figureTree
+    },
+    expected: false
+  },
+  {
+    name: 'Move up',
+    opts: {
+      actor: playerSoul,
+      actionOpts: {
+        vector: [0, -1]
+      },
+      figureTree: figureTree
+    },
+    expected: false
+  },
+  {
+    name: 'Move down',
+    opts: {
+      actor: playerSoul,
+      actionOpts: {
+        vector: [0, 1]
+      },
+      figureTree: figureTree
+    },
+    expected: true
+  }
+];
+
+var executeTestCases = [
+  {
+    name: 'Move left',
+    opts: {
+      actor: playerSoul,
+      actionOpts: {
+        vector: [-1, 0]
+      },
+      figureTree: figureTree
+    },
+    expectedResult: true,
+    expectedActorPositions: [
+      {
+        minX: 8,
+        maxX: 9,
+        minY: 4,
+        maxY: 5
+      }
+    ]
+  },
+  {
+    name: 'Move right',
+    opts: {
+      actor: playerSoul,
+      actionOpts: {
+        vector: [1, 0]
+      },
+      figureTree: figureTree
+    },
+    expectedResult: true,
+    expectedActorPositions: [
+      {
+        minX: 9,
+        maxX: 10,
+        minY: 4,
+        maxY: 5
+      }
+    ]
+  },
+  {
+    name: 'Move up',
+    opts: {
+      actor: playerSoul,
+      actionOpts: {
+        vector: [0, -1]
+      },
+      figureTree: figureTree
+    },
+    expectedResult: false,
+    expectedActorPositions: [
+      {
+        minX: 9,
+        maxX: 10,
+        minY: 4,
+        maxY: 5
+      }
+    ]
+  },
+  {
+    name: 'Move down',
+    opts: {
+      actor: playerSoul,
+      actionOpts: {
+        vector: [0, 1]
+      },
+      figureTree: figureTree
+    },
+    expectedResult: true,
+    expectedActorPositions: [
+      {
+        minX: 9,
+        maxX: 10,
+        minY: 5,
+        maxY: 6
+      }
+    ]
+  }
+];
+
+canDoTestCases.forEach(runCanDoTest);
+executeTestCases.forEach(runExecuteTest);
+
+function runCanDoTest(testCase) {
+  test('canDo ' + testCase.name, moveCanDoTest);
+
+  function moveCanDoTest(t) {
+    move.canDo(testCase.opts, checkCanDo);
+
+    function checkCanDo(error, result) {
+      assertNoError(t.ok, error, 'No error from canDo.');
+      t.equal(result, testCase.expected, 'canDo result is correct.');
+      t.end();
+    }
+  }
+}
+
+function runExecuteTest(testCase) {
+  test('execute ' + testCase.name, moveExecuteTest);
+
+  function moveExecuteTest(t) {
+    move.execute(testCase.opts, checkExecute);
+
+    function checkExecute(error, result) {
+      assertNoError(t.ok, error, 'No error from canDo.');
+      t.equal(result, testCase.expectedResult, 'canDo result is correct.');
+      t.deepEqual(
+        testCase.opts.actor.figures.map(f => pick(f, 'minX', 'maxX', 'minY', 'maxY')),
+        testCase.expectedActorPositions,
+        'Actor figures\' positions are correct after executing.'
+      );
+      t.end();
+    }
+  }
+}
